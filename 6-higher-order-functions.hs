b303bd0d5380f12a35600757c4ad3337b0080043
MpoMp <mpomp.aka.freestyler@gmail.com>
2014-09-11 15:55:09 +0300

    6th completed

--- 6-higher-order-functions.hs
+++ 6-higher-order-functions.hs
@@ -2,30 +2,31 @@
 -- Example: sumInts 0 1 = 1
 --          sumInts 1 3 = 6
 sumInts :: Int -> Int -> Int
-sumInts a b = undefined
+sumInts a b = foldl1 (+) [a .. b]
+
 
 -- Define a square function
 sq :: Int -> Int
-sq x = undefined
+sq = (^2)
 
 -- Sum the squares between two numbers. This function should be similar to the sumInts function
 sumSquares :: Int -> Int -> Int
-sumSquares a b = undefined
+sumSquares a b = foldl1 (+) $ map sq [a .. b]
 
 -- Define a higher order sum function which accepts an (Int -> Int) function to apply to all integers between two values.
 -- Again this should look similar to the sumInts and sumSquares functions
 higherOrderSum :: (Int -> Int) -> Int -> Int -> Int
-higherOrderSum intApplication a b = undefined
+higherOrderSum intApplication a b = foldl1 (+) $ map intApplication [a .. b]
 
 -- Define the square sum in terms of higherOrderSum
 hoSumSquares :: Int -> Int -> Int
-hoSumSquares = undefined
+hoSumSquares = higherOrderSum sq 
 
 -- Define the sum between two values in terms of higherOrderSum
 -- Note there is no parameter on the function definition
 -- Try to use a lambda if possible
 hoSumInts :: Int -> Int -> Int
-hoSumInts = undefined
+hoSumInts = higherOrderSum (\a -> a)
 
 -- Create a new higher order method which generalises over the function provided by sumInts (That is, parameterize (+) :: Int -> Int -> Int) between a and b
 -- This will give the ability to perform utilities such as the prodcut of all squares (or any other Int -> Int function) between a and b
@@ -33,12 +34,16 @@ hoSumInts = undefined
 -- You can also define the function signature yourself, which leaves you free to define the parameters and their order
 -- To be clear, your function will need to handle:
 --  - A start value, a :: Int
---  - A end value, b :: Int
+--  - An end value, b :: Int
 --  - A function to apply to each value, op :: Int -> Int
 --  - A function to apply between each value, f :: Int -> Int -> Int
 --  - A value to return in the base case when a > b, z :: Int
-higherOrderSequenceApplication = undefined
+higherOrderSequenceApplication :: Int -> Int -> (Int -> Int) -> (Int -> Int -> Int) -> Int -> Int
+higherOrderSequenceApplication a b op f z = if a > b 
+                                            then z 
+                                            else foldl1 f $ map op [a .. b]
+
 
 -- Define a factorial method using the higherOrderSequenceAppliction
-hoFactorial :: Int -> Int
-hoFactorial = undefined
+hoFactorial :: Int -> Int -- needs another type, overflows pretty soon
+hoFactorial a = higherOrderSequenceApplication 1 a (1*) (*) 1
