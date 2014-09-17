c4b7cab9fd53fe4b65fbce3ebe8d67aa1106c1bf
MpoMp <mpomp.aka.freestyler@gmail.com>
2014-09-17 14:02:06 +0300

    8 done

--- 8-making-our-own-types-and-typeclasses.hs
+++ 8-making-our-own-types-and-typeclasses.hs
@@ -1,3 +1,4 @@
+import Data.List
 {-
  - We are going to create some types for a deck of cards
  - The cards need to have an ordering, based on the standard ranking http://en.wikipedia.org/wiki/Standard_52-card_deck#Rank_and_color
@@ -12,28 +13,39 @@
  - Uncomment the following declarations to complete the implementation, and provide an implementation for instance Show Card
  -}
 
---data Suit = 
---data Digit = 
---data Card = 
+data Suit = Clubs | Diamonds | Hearts | Spades deriving (Eq, Show)
+data Digit = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten 
+            | Jack | Queen | King | Ace deriving (Eq, Ord, Bounded, Show)
+            
+data Card = Card    { digit :: Digit
+                    , suit :: Suit
+                    } deriving (Eq)
+
+instance Show Card where  
+    show c = "The " ++ show (digit c) ++ " of " ++ show (suit c)
+
 
 -- We should be able to provide a function which returns the higher ranked card:
 betterCard :: Card -> Card -> Card
-betterCard x y = undefined
+betterCard x y = if digit x > digit y then x else y
 
 -- Here is a new Typeclass, which represents some kind of playing hand in a game.
 -- It returns True for a "winning hand", depending on the rules for the type of class we are playing with
 class Hand a where
     play :: [a] -> Bool
+    
 
 -- Implement Hand for Card, where play returns true if the list contains the Ace of Spades
 instance Hand Card where
-    play c = undefined
+    play [] = False -- this does not work for "play []" but only "play ([] :: Card)"
+    play c = elem (Card Ace Spades) c
 
 -- Create a new Coin type
---data Coin = 
+data Coin = Tails | Heads deriving (Ord, Eq, Show)
 
 -- Implement Hand for Coin, where play returns true if there are ten heads in a row in the list
 instance Hand Coin where
-	play c =  undefined
+    play [] = False -- this does not work for "play []" but only "play ([] :: Coin)"
+    play c = isInfixOf (replicate 10 Heads) c
 
 -- Have a play with implementing Hand for some other types, for instance Int and Bool
